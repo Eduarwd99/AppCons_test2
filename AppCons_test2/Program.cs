2be54cb47e07fc96ca4c841568d964193f971d27
G <G@PC>
2020-05-25 09:56:24 -0500

    Revert "Bucle For"
    This reverts commit 7da6b6b467fa2e41f52418de34211f94cf443c92.

--- AppCons_test2/Program.cs
+++ AppCons_test2/Program.cs
@@ -12,8 +12,6 @@ namespace AppCons_test2
 		[STAThread]
 		static void Main()
 		{
-			//Estructura Secuencial
-
 			int a; a = 10;
 			int b; b= 20;
 			Console.WriteLine("El producto de {0} X {1} es: {2}" a,b,(a*b));
@@ -30,18 +28,6 @@ namespace AppCons_test2
 			{
 				Console.WriteLine("{0} es mayor", b);
 			}
-
-			//Bucles
-			//Bucle for
-
-			for (int i = 1; i <= 10; i++) ;
-			{
-				Console.WriteLine("Hola {0}", i);
-			}
-			Console.WriteLine("Pulse una tecla para continuar...");
-			Console.ReadKey();
-
-
 		}
 	}
 }
